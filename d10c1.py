c28834f722c59f58aaf64667ccf9aeae37e1e6b9
madhav84garg-gif <madhav84garg@gmail.com>
2026-02-21 20:10:45 +0530

    python Programming practice files

---
+++ d10c1.py
@@ -0,0 +1,6 @@
+num = int(input("Enter number:"))
+
+if num % 3 == 0 and num % 7 == 0:
+    print("Divisible by both 3 and 7")
+else:
+    print("Not divisible by both")  
---
+++ d11c1.py
@@ -0,0 +1,8 @@
+ch = input("Enter a character:")
+
+if ch.isupper():
+    print("Uppercase")
+elif ch.islower():
+    print("Lowercase")
+else:
+    print("Not an alphabet")
